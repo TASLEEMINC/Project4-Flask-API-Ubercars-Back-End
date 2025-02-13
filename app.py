9ed55a92d686c7ce7155e9b8b024f6d6d198187c
vitasmile <vitasmile@gmail.com>
2025-02-12 21:08:06 -0500

    Sign-in/up added, JWT Authentication implemented

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import os
+from auth_middleware import token_required
+import bcrypt
+import jwt
 import psycopg2, psycopg2.extras
 from dotenv import load_dotenv
-from flask import Flask, request
+import os
+from flask import Flask, jsonify, request, g
 from flask_cors import CORS
 
 load_dotenv()
@@ -91,5 +94,109 @@ def update_ubercar(ubercar_id):
       return updated_ubercar, 202
     except Exception as e:
       return str(e), 500
+
+@app.route('/sign-token', methods=['GET'])
+def sign_token():
+    user = {
+        "id": 1,
+        "username": "test",
+        "password": "test"
+    }
+    token = jwt.encode(user, os.getenv('JWT_SECRET'), algorithm="HS256")
+    return jsonify({"token": token})
+
+@app.route('/verify-token', methods=['POST'])
+def verify_token():
+    try:
+        token = request.headers.get('Authorization').split(' ')[1]
+        decoded_token = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=["HS256"])
+        return jsonify({"user": decoded_token})
+    except Exception as err:
+       return jsonify({"err": err.message})
+
+@app.route('/auth/sign-up', methods=['POST'])
+def sign_up():
+    try:
+        # Get data from form
+        new_user_data = request.get_json()
+
+        # Find user in DB
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (new_user_data["username"],))
+        existing_user = cursor.fetchone()
+
+        # Dont create a duplicate account
+        if existing_user:
+            cursor.close()
+            return jsonify({"error": "Username already taken"}), 400
+
+        # Encrypt the raw password
+        hashed_password = bcrypt.hashpw(bytes(new_user_data["password"], 'utf-8'), bcrypt.gensalt())
+
+        # Create the new user in DB
+        cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id, username", (new_user_data["username"], hashed_password.decode('utf-8')))
+        created_user = cursor.fetchone()
+        connection.commit()
+        connection.close()
+
+        # Construct the payload
+        payload = {"username": created_user["username"], "id": created_user["id"]}
+        # Create the token, attaching the payload
+        token = jwt.encode({ "payload": payload }, os.getenv('JWT_SECRET'))
+        # Send the token instead of the user
+        return jsonify({"token": token}), 201
+    except Exception as err:
+        return jsonify({"err": str(err)}), 401
+
+
+@app.route('/auth/sign-in', methods=["POST"])
+def sign_in():
+    try:
+        sign_in_form_data = request.get_json()
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM users WHERE username = %s;", (sign_in_form_data["username"],))
+        existing_user = cursor.fetchone()
+        if existing_user is None:
+            return jsonify({"err": "Invalid credentials."}), 401
+        password_is_valid = bcrypt.checkpw(bytes(sign_in_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+        if not password_is_valid:
+            return jsonify({"err": "Invalid credentials."}), 401
+        
+
+        # Construct the payload
+        payload = {"username": existing_user["username"], "id": existing_user["id"]}
+        # Create the token, attaching the payload
+        token = jwt.encode({ "payload": payload }, os.getenv('JWT_SECRET'))
+        # Send the token instead of the user
+        return jsonify({"token": token}), 200
+    
+    except Exception as err:
+        return jsonify({"err": err.message}), 500
+    finally:
+        connection.close()
+
+@app.route('/users')
+@token_required
+def users_index():
+    connection = get_db_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT id, username FROM users;")
+    users = cursor.fetchall()
+    connection.close()
+    return jsonify(users), 200
+
+@app.route('/users/<user_id>')
+@token_required
+def users_id(user_id):
+    connection = get_db_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT id, username FROM users WHERE id = %s;", (user_id))
+    user = cursor.fetchone()
+    connection.close()
+    if user is None:
+        return jsonify({"err": "User not found"}), 404
+    return jsonify(user), 200
 
 app.run()
