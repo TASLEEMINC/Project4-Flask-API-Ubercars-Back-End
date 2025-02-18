22b2223f8967079df9255a158a274ccbc2f4821e
vitasmile <vitasmile@gmail.com>
2025-02-18 02:40:43 -0500

    updated database_url/uri

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ def get_db_connection():
             os.getenv('DATABASE_URL'), 
             sslmode='require'
         )
-
     else:
         connection = psycopg2.connect(
             host='localhost',
-            database='ubercars_db',
-            user=os.environ['POSTGRES_USER'],
-            password=os.environ['POSTGRES_PASSWORD']
+            database=os.getenv('POSTGRES_DATABASE'),
+            user=os.getenv('POSTGRES_USERNAME'),
+            password=os.getenv('POSTGRES_PASSWORD')
         )
     return connection
+
 
 @app.route('/')
 def index():
